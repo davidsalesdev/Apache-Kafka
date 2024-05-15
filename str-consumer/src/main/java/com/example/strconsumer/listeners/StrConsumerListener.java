54d38ebc8084a515a82679f6779f10e18fde5fb8
davidsalesdev <b4rbab4rba@gmail.com>
2024-05-14 21:15:56 -0300

    subindo aplicação de consumo e producer kafka

---
+++ str-consumer/src/main/java/com/example/strconsumer/listeners/StrConsumerListener.java
@@ -0,0 +1,31 @@
+package main.java.com.example.strconsumer.listeners;
+
+import com.example.strconsumer.custom.StrConsumerCustomListener;
+
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class StrConsumerListener {
+
+    @SneakyThrows
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message);
+        throw new IllegalArgumentException("EXCEPTION...");
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive message {}", message);
+    }
+
+}
